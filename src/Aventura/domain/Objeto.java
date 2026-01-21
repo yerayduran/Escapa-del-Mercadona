87f59d3609f773eed9316bd1d2056585ed551c6d
yerayduran <yerayduranchaves2006@gmail.com>
2026-01-21 09:14:00 +0100

    Estructura de Objeto

---
+++ src/Aventura/domain/Objeto.java
@@ -0,0 +1,19 @@
+package Aventura.domain;
+
+public abstract class Objeto extends Entidad{
+
+    private boolean visible;
+
+    public Objeto(String nombre, String descripcion, boolean visible) {
+        super(nombre, descripcion);
+        setVisible(visible);
+    }
+
+    public boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+}
