55f101731f815b698705b13cbb515dd6e890828a
Manu <109155188+Manolilloo@users.noreply.github.com>
2026-01-22 13:35:00 +0100

    mueble

--- src/aventura/domain/Item.java
+++ src/aventura/domain/Item.java
@@ -7,11 +7,11 @@ package aventura.domain;
 
 import aventura.interfaces.Inventariable;
 
+
 public class Item extends Objeto implements Inventariable {
 
     public Item(String nombre, String descripcion) {
         super(nombre, descripcion);
-        // Por defecto, un item suele ser visible al crearse
         this.setVisible(true);
     }
 }
